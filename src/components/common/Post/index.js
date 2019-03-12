00407ef5ff82e75f5fa1845dd49f70470aca92a5
doan anh <anh.doan@enouvo.com>
2019-03-12 21:08:12 +0700

    fix error

--- src/components/common/Post/index.js
+++ src/components/common/Post/index.js
@@ -7,43 +7,45 @@ import {
  ArticleWrapper, Back, Content, Comments, ArticleDate,
 } from './styles';
 
-export const Post = ({ html, frontmatter, timeToRead }) => (
-  <ThemeContext.Consumer>
-    {({ theme }) => {
-      const disqusShortName = 'https-doananh234-com';
-      const disqusConfig = {
-        url: frontmatter.url,
-        identifier: frontmatter.id,
-        title: frontmatter.title,
-      };
-      return (
-        <ArticleWrapper theme={theme}>
-          <PageTitle>{frontmatter.title}</PageTitle>
-          <ArticleDate>
-            <i>
-              {frontmatter.date}
-              {' '}
+export const Post = ({ html, frontmatter, timeToRead }) => {
+  return (
+    <ThemeContext.Consumer>
+      {({ theme }) => {
+        const disqusShortName = 'https-doananh234-com';
+        const disqusConfig = {
+          url: frontmatter.path,
+          identifier: frontmatter.id,
+          title: frontmatter.title,
+        };
+        return (
+          <ArticleWrapper theme={theme}>
+            <PageTitle>{frontmatter.title}</PageTitle>
+            <ArticleDate>
+              <i>
+                {frontmatter.date}
+                {' '}
 -
-            </i>
-            <i>
-              {timeToRead}
-              {' '}
+              </i>
+              <i>
+                {timeToRead}
+                {' '}
 min read
-            </i>
-          </ArticleDate>
-          <Content dangerouslySetInnerHTML={{ __html: html }} />
-          <SocialShare {...frontmatter} />
-          <Back>
-            <Link to={frontmatter.next}>Previous article</Link>
-          </Back>
-          <Comments>
-            <Disqus.DiscussionEmbed shortname={disqusShortName} config={disqusConfig} />
-          </Comments>
-        </ArticleWrapper>
-      );
-    }}
-  </ThemeContext.Consumer>
-);
+              </i>
+            </ArticleDate>
+            <Content dangerouslySetInnerHTML={{ __html: html }} />
+            <SocialShare {...frontmatter} />
+            <Back>
+              <Link to={frontmatter.next}>Previous article</Link>
+            </Back>
+            <Comments>
+              <Disqus.DiscussionEmbed shortname={disqusShortName} config={disqusConfig} />
+            </Comments>
+          </ArticleWrapper>
+        );
+      }}
+    </ThemeContext.Consumer>
+  );
+};
 
 Post.propTypes = {
   html: PropTypes.any,
--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import { Layout } from '../components/common/Layout';
-import Post from '../components/common/Post';
+import { Post } from '../components/common/Post';
 
 const PostTemplate = ({ data }) => {
   const { title: siteTitle, subtitle: siteSubtitle } = data.site.siteMetadata;
 
   const { title: postTitle, description: postDescription } = data.markdownRemark.frontmatter;
-
   const metaDescription = postDescription !== null ? postDescription : siteSubtitle;
 
   return (
