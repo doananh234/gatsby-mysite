c4ad91e49bdbbd61b3b73968c818f242089d5e7a
Anh <doananh234@gmail.com>
2020-08-03 23:56:24 +0700

    remove chat box

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,32 +14,6 @@ const IndexPage = () => (
       <Services />
       <Work />
       <Github />
-      <div id="fb-root" />
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `<script>
-        window.fbAsyncInit = function() {
-          FB.init({
-            xfbml            : true,
-            version          : 'v7.0'
-          });
-        };
-
-        (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));</script>`,
-        }}
-      />
-      <div
-        className="fb-customerchat"
-        attribution="setup_tool"
-        page_id="236598804811289"
-        theme_color="#F47D25"
-      />
     </Layout>
   </ThemeProvider>
 )
